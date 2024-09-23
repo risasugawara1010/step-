4f67bd34062faaa69f03ad0eab9926b0b8b6e7d1
risa sugawara <r.s.black1010@gmail.com>
2024-09-23 13:22:04 +0900

    ‘message’

--- routes/web.php
+++ routes/web.php
@@ -2,37 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Auth;
-
-
-
-Route::get('/', function () {
-    
-    if (Auth::check()) {
-        
-        return redirect()->route('products.index');
-        
-    } else {
-        
-        return redirect()->route('login');
-        
-    }
-});
-
 
 Auth::routes();
 
+Route::get('/', function () {
+    return redirect()->route('login');
+})->name('root');
 
+Route::get('/home', [ProductController::class, 'index'])->name('home');
 
 Route::group(['middleware' => 'auth'], function() {
-
-    Route::get('/', [ProductController::class, 'index'])->name('products.index');
-    Route::post('/search', [ProductController::class, 'search'])->name('products.search');
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/search', [ProductController::class, 'search'])->name('products.search');
     Route::get('/create', [ProductController::class, 'create'])->name('products.create');
     Route::post('/store', [ProductController::class, 'store'])->name('products.store');
     Route::get('/show/{product}', [ProductController::class, 'show'])->name('products.show');
     Route::get('/edit/{product}', [ProductController::class, 'edit'])->name('products.edit');
     Route::put('/update/{product}', [ProductController::class, 'update'])->name('products.update');
     Route::delete('/products/delete/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-
 });
